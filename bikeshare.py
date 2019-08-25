03e056c8c5c291063179ca359e9e105d2e7f8844
Ganira Gozalova <ganiragozalova@Ganiras-MacBook-Air.local>
2019-08-25 17:49:25 +0100

    style: formatting- no code change

--- bikeshare.py
+++ bikeshare.py
@@ -196,7 +196,7 @@ def birth_year(df):
         print("\nThis took %s seconds." % (time.time() - start_time))
         print('-'*40)
 
-    #Display 5 rows of raw data 
+    #Display 5 rows of raw data and display the rest of 5 rows if requested
 def display_data (df):
     start=0
     end=5
